fa68895e666e57fd14eec4cb82a191dcd2634464
Eruntinco <49297205+Eruntinco@users.noreply.github.com>
2019-04-05 11:45:56 -0400

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,16 @@
+
+public class Game {
+
+	public String title;
+	public String description;
+	public String publisher;
+	public String genre;
+	public String releaseDate;
+	
+	public int rating;
+	
+	public Review[] reviews;
+	
+	public String youtubeLink;
+	
+}
---
+++ Review.java
@@ -0,0 +1,9 @@
+
+public class Review {
+
+	UserAccount submittedUser;
+	
+	String text;
+	String date;
+	int rating;
+}

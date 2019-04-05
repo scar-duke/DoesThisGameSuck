584f6c28137a12eff010e3b9b132fa70ab408fb9
Eruntinco <49297205+Eruntinco@users.noreply.github.com>
2019-04-05 11:47:47 -0400

    Add files via upload
    Added files in the src folder instead.

---
+++ src/Wishlist.java
@@ -0,0 +1,17 @@
+
+public class Wishlist {
+
+	public Game[] gameList;
+	
+	public String listowner; //Change to UserAccount?
+	
+	private boolean visability;
+
+	public boolean addGame() {		//There is the addToWishlist method in UserAccount, why does addGame exist?
+		
+	}
+	
+	public boolean removeGame() {	//Should we have a UserAccount method for removing games as well?
+		
+	}
+}

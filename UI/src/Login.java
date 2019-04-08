b05fbb0525aec7a8fd0ddda8ed3e62593fc5d8eb
caoz7 <caoz7@miamioh.edu>
2019-04-07 20:59:53 -0400

    Update Login.java
    sucks to suck

--- UI/src/Login.java
+++ UI/src/Login.java
@@ -116,7 +116,7 @@ public class Login {
 		password.setBounds(160, 167, 264, 23);
 		frame.getContentPane().add(password);
 		
-		JLabel lblDoesThisGames = new JLabel("Does This Games Sucks");
+		JLabel lblDoesThisGames = new JLabel("Does This Games Suck");
 		lblDoesThisGames.setFont(new Font("Tahoma", Font.BOLD, 17));
 		lblDoesThisGames.setBounds(174, 37, 234, 36);
 		frame.getContentPane().add(lblDoesThisGames);

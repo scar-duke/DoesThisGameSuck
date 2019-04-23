0564a091856768765900993bff86d00596e54558
Eruntinco <49297205+Eruntinco@users.noreply.github.com>
2019-04-23 12:49:37 -0400

    Initial tester for the review class

---
+++ UI/src/ReviewTester.java
@@ -0,0 +1,60 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+
+class ReviewTester {
+
+	@Test
+	void testReview() {														//Assumes that there are no pre-existing reviews
+		
+		connection = connect.dbConnector();									//Establishes a connection so that data can be pulled from the database
+		
+		Review test0 = new Review(0, "GAMETEST0", 0, "USERTEST0", 3);		//Why are the names used in the constructor, rather than just the ids?
+		
+		String result = null;												//Defined as the scope of variables declared in try is limited to that block
+		
+		try {
+			String query = "SELECT * FROM reviews;";						//Query to acquire the new review from the database
+			PreparedStatement pst = connection.prepareStatement(query);	
+			ResultSet rs = pst.executeQuery();
+			rs.next();
+			String result = rs.getString(1);
+			rs.close();
+			pst.close();
+		} 
+		
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		assertTrue(result.length() > 0);									//Conditional to ensure that a review exists in the database
+	}
+	
+	void testGetRating() {
+		Review test1 = new Review(1, "GAMETEST1", 1, "USERTEST1", 3);		//Various definitions and checks to ensure that the correct review average is returned
+		
+		assertEquals(3, Review.getRating(1));
+		
+		Review test2 = new Review(1, "GAMETEST1", 2, "USERTEST2", 5);
+
+		assertEquals(4, Review.getRating(1));
+
+		Review test3 = new Review(1, "GAMETEST1", 3, "USERTEST3", 2);
+		
+		assertEquals(3, Review.getRating(1));
+		
+		Review test4 = new Review(1, "GAMETEST1", 4, "USERTEST4", 1);
+		
+		assertEquals(2, Review.getRating(1));
+		
+		Review test5 = new Review(2, "GAMETEST2", 5, "USERTEST5", 5);		//Definition and check for one game's average not affected that of another game
+		
+		assertEquals(5, Review.getRating(2));
+	}
+
+}

2d36e97b8d6bcd6c4b96fa9c6ac7b2b1a92b1126
Eruntinco <49297205+Eruntinco@users.noreply.github.com>
2019-04-23 21:08:46 -0400

    Added @BeforeEach method to remove preexisting reviews

--- UI/src/ReviewTester.java
+++ UI/src/ReviewTester.java
@@ -5,14 +5,31 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ReviewTester {
 
+	@BeforeEach
+	void cleanTable() {
+		Connection connection = connect.dbConnector();
+		
+		try {
+			String query = "DELETE FROM reviews;";						//Query to clean database of preexisting reviews
+			PreparedStatement pst = connection.prepareStatement(query);	
+			pst.executeUpdate();
+			pst.close();
+		} 
+		
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Test
 	void testReview() {														//Assumes that there are no pre-existing reviews
 		
-		connection = connect.dbConnector();									//Establishes a connection so that data can be pulled from the database
+		Connection connection = connect.dbConnector();									//Establishes a connection so that data can be pulled from the database
 		
 		Review test0 = new Review(0, "GAMETEST0", 0, "USERTEST0", 3);		//Why are the names used in the constructor, rather than just the ids?
 		
@@ -35,7 +52,7 @@ class ReviewTester {
 		assertTrue(result.length() > 0);									//Conditional to ensure that a review exists in the database
 	}
 	
-	@test
+	@Test
 	void testGetRating() {
 		Review test1 = new Review(1, "GAMETEST1", 1, "USERTEST1", 3);		//Various definitions and checks to ensure that the correct review average is returned
 		
@@ -55,7 +72,7 @@ class ReviewTester {
 		
 		Review test4b = new Review(1, "GAMETEST1", 4, "USERTEST4", 5);
                 
-                assertEquals(2, Review.getRating(1));
+        assertEquals(2, Review.getRating(1));
 		
 		Review test5 = new Review(2, "GAMETEST2", 5, "USERTEST5", 5);		//Definition and check for one game's average not affected that of another game
 		
